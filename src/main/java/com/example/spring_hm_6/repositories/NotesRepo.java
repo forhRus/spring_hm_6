d5082d27447618ec7512af9282c6c3744524c6b1
Sergey Shkaev <shkaev.ss@mail.ru>
2024-01-29 20:36:08 +0300

    создан интерфейс NotesRepo наследует от JpaRepository

---
+++ src/main/java/com/example/spring_hm_6/repositories/NotesRepo.java
@@ -0,0 +1,10 @@
+package com.example.spring_hm_6.repositories;
+
+import com.example.spring_hm_6.entyties.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+
+public interface NotesRepo extends JpaRepository<Note, Long> {
+}
